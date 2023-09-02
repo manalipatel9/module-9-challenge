71ef825567f555852393147a90608401a044cc42
manalipatel9 <136845837+manalipatel9@users.noreply.github.com>
2023-09-02 13:28:17 -0500

    Add files via upload

---
+++ module 9 challenge/EmployeeSQL/ERD image.sql
@@ -0,0 +1,43 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/mLopIT
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+CREATE TABLE "department" (
+    "dept_no" string   NOT NULL,
+    "dept_name" string   NOT NULL
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" int   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" string   NOT NULL,
+    "emp_no" int   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" string   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" string   NOT NULL,
+    "last_name" string   NOT NULL,
+    "sex" string   NOT NULL,
+    "hire_date" date   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" string   NOT NULL,
+    "title" string   NOT NULL
+);
+
